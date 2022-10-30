8d4a3db5f6d50cf28ba5c631c4f3ec9699b2681b
Veera P <31029098+veerap000@users.noreply.github.com>
2022-10-30 18:29:10 +0200

    Run frontend script in footer

--- public/class-overlay-search-public.php
+++ public/class-overlay-search-public.php
@@ -93,7 +93,7 @@ class Overlay_Search_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/overlay-search-public.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/overlay-search-public.js', array( 'jquery' ), $this->version, true );
 
 	}
 
